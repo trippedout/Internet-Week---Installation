cbc830fe7f38c0c48e96aabb860491d8a696b767
trippedout <anthony.tripaldi@gmail.com>
2011-05-24 12:26:16 -0400

    refactored kinect out of installation

--- src/com/lbi/internetweek/Installation.java
+++ src/com/lbi/internetweek/Installation.java
@@ -8,14 +8,13 @@ import hypermedia.video.*;
 import com.lbi.internetweek.utils.Stats;
 import com.lbi.internetweek.views.Background;
 import com.lbi.internetweek.views.Bird;
+import com.lbi.internetweek.views.KinectWrapper;
 
 import toxi.geom.Rect;
 import toxi.geom.Vec2D;
 import toxi.physics2d.behaviors.*;
 import toxi.physics2d.VerletParticle2D;
 import toxi.physics2d.VerletPhysics2D;
-
-import SimpleOpenNI.*;
 
 public class Installation extends PApplet 
 {
@@ -47,11 +46,8 @@ public class Installation extends PApplet
 	//Flock flock;
 
 	//kinect
-	SimpleOpenNI    kinect;
-	PImage          depthImage;
-	int[]           rawDepth;
-	int             kWidth, kHeight;
-	int             nearThreshold, farThreshold;
+	KinectWrapper	kinect;
+	//
 
 	//openCV
 	OpenCV          opencv;
@@ -77,27 +73,20 @@ public class Installation extends PApplet
 	{
 		//processing calls
 		//-------------------------------------
-		size( 1280, 960, OPENGL );
-		shapeMode(CENTER);
-		  
-		//vars
-		//-------------------------------------
-		nearThreshold      =  0;
-		farThreshold       =  2500;
+		size( 1280, 960, OPENGL );		
 		  
 		//image loading
 		//-------------------------------------
-		pi_Bg        =  loadImage( "bg.jpg" );
 		pi_Bird      =  loadImage( "bird_sprites.png" );
-		depthImage   =  createImage( 320, 240, RGB );
 		  
 		//instantiation
 		//-------------------------------------
 		bg			=	new Background(this);
-		setupKinect();
+		kinect		=	new KinectWrapper(this);
+		
 		setupOpenCV();
 		setupPhysics();
-		setupFlock();
+		setupBirds();
 		  
 		//extras
 		//-------------------------------------
@@ -107,151 +96,94 @@ public class Installation extends PApplet
 	
 	public void draw()
 	{
-		
-		//initial calls
-		//-------------------------------------
 		bg.draw();
 		
-		kinect.update();  
-		drawKinect();
+		kinect.update();
+		kinect.draw();
+		
+		drawOpenCV();
 		 
 		checkPhysics();
 		physics.update();
 		drawBirds();
 		  
-		//classes
-		//-------------------------------------
 		
-		//flock.run();
-		 
-		//extras
-		//-------------------------------------
 		//if( mousePressed ) 
 			_stats.draw(0,0);
 	}
-	
+
 	// --------------------------------------------------------------------------------------------------------
 	// SETUP FUNCTIONS
 	// --------------------------------------------------------------------------------------------------------
 
-	void setupKinect()
-	{
-		kinect = new SimpleOpenNI(this,SimpleOpenNI.RUN_MODE_MULTI_THREADED); 
-		kinect.setMirror(true);
-		kinect.enableDepth();
-	}
-
 	void setupOpenCV()
 	{
-	  opencv       =  new OpenCV(this);  
-	  opencv.allocate( depthImage.width, depthImage.height );
+		opencv       =  new OpenCV(this);  
+		opencv.allocate( kinect.getDepthImage().width, kinect.getDepthImage().height );
 	}
 
 	void setupPhysics()
 	{
-	  physics = new VerletPhysics2D();
-	  physics.setDrag(0.005f);
+		physics = new VerletPhysics2D();
+		physics.setDrag(0.005f);
 	  
-	  physics.setWorldBounds(new Rect(50, 50, width-100, height-100));  
-	  //physics.addBehavior(new GravityBehavior(new Vec2D(0, 0.05f)));
+		physics.setWorldBounds(new Rect(50, 50, width-100, height-100));  
+		//physics.addBehavior(new GravityBehavior(new Vec2D(0, 0.05f)));
 	  
-	  for( int i = 0; i < numBirds; ++i )
-	  {
-	    VerletParticle2D p = new VerletParticle2D(new Vec2D(random(width-100)+50, random(height-100)+50));
-	    physics.addParticle(p);
-	  }
+		for( int i = 0; i < numBirds; ++i )
+		{
+			VerletParticle2D p = new VerletParticle2D(new Vec2D(random(width-100)+50, random(height-100)+50));
+			physics.addParticle(p);
+		}
 	  
-	  for( int i = 0; i < attractionPool.length; ++i )
-	  {
-	    Vec2D v = new Vec2D( -1000, -1000 );
-	    attractionPool[i] = new AttractionBehavior(v, 250.0f, 0.0f);
-	    physics.addBehavior(attractionPool[i]);
-	  }
+		for( int i = 0; i < attractionPool.length; ++i )
+		{
+			Vec2D v = new Vec2D( -1000, -1000 );
+			attractionPool[i] = new AttractionBehavior(v, 250.0f, 0.0f);
+			physics.addBehavior(attractionPool[i]);
+		}
 	}
 
-	void setupFlock()
+	void setupBirds()
 	{
-	  //flock = new Flock();
-	  
-	  for (int i = 0; i < numBirds; ++i) 
-	  {
-		  birds[i]		=	new Bird(this, pi_Bird);
-	    //flock.addBoid( new Boid( new Vec2D(width/2,height/2), 3.0, 0.05 ) );
-	  }
+		for (int i = 0; i < numBirds; ++i) 
+		{
+			birds[i]		=	new Bird(this, pi_Bird);
+		}
 	}
 
 	// --------------------------------------------------------------------------------------------------------
 	// DRAW FUNCTIONS
 	// --------------------------------------------------------------------------------------------------------
 
-	void drawKinect()
+	void drawOpenCV()
 	{
-	  int i, j, c, dpos, pos;
-	  int w=640, h=480;
-	  
-	  if( frameCount % 4 == 0 )
-	  {
-	    dpos = 0;
-	    pos = 0;
-	    
-	    rawDepth = kinect.depthMap();
-	    
-	    for( i = 0; i < h; i += 2 )
-	    {
-	      for( j = 0; j < w; j += 2 )
-	      {
-	        dpos   =  ( i * w ) + j;
-	        
-	        c      =  rawDepth[dpos];
-	                
-	        if( c > nearThreshold && c < farThreshold )
-	          depthImage.pixels[pos] = 0xffffff;
-	        else
-	          depthImage.pixels[pos] = 0;
-	        
-	        pos++;
-	      }
-	    }
-	    
-	    depthImage.updatePixels();
-	  }
-	  
-	  /*
-	  //draw depthImage quickly on screen
-	  beginShape(QUADS);
-	  texture(depthImage);
-	  vertex(0,0,0,0);
-	  vertex(320,0,320,0);
-	  vertex(320,240,320,240);
-	  vertex(0,240,0,240);
-	  endShape();
-	  //*/
-	  
-	  //openCV blob analysis
-	  opencv.copy( depthImage );  
-	  blobs = opencv.blobs( 75*75, 320*240, 50, false );
-	  
-	  if( blobs.length > 0 )
-	  {  
-	      blob = blobs[0];
-	      
-	     if( DRAW_KINECT_BLOBS )
-	     {
-	        fill(0,255,0,100);
-	        noStroke();
-	        int nx, ny;
-	        
-	        beginShape();        
-	        for( i = 0; i < blob.points.length; i += 2 ) 
-	        {
-	            nx = parseInt( map( blob.points[i].x, 0, 320, 0, width ) );
-	            ny = parseInt( map( blob.points[i].y, 0, 240, 0, height ) );
-	          
-	            vertex( nx, ny );
-	        }
-	        endShape(CLOSE);
-	     }
-	  }
+		opencv.copy( kinect.getDepthImage() );  
+		
+		//arbitrary size numbers for finding blobs big enough to be people
+		blobs = opencv.blobs( 75*75, 320*240, 10, false );
+  
+		if( blobs.length > 0 )
+		{  
+			blob = blobs[0];
+      
+			if( DRAW_KINECT_BLOBS )
+			{
+				fill(0,255,0,100);
+				noStroke();
+				int nx, ny;
+        
+				beginShape();        
+				for( int i = 0; i < blob.points.length; i += 2 ) 
+				{
+					nx = parseInt( map( blob.points[i].x, 0, 320, 0, width ) );
+					ny = parseInt( map( blob.points[i].y, 0, 240, 0, height ) );
+          
+					vertex( nx, ny );
+				}
+				endShape(CLOSE);
+			}
+			}
 	  else
 	  {
 		  blob = null;
@@ -321,6 +253,40 @@ public class Installation extends PApplet
 			VerletParticle2D p = physics.particles.get(i);
 			birds[i].draw(p);
 		}
+	}
+
+	// --------------------------------------------------------------------------------------------------------
+	// KINECT HELPER FUNCS
+	// --------------------------------------------------------------------------------------------------------
+	
+	public void onNewUser(int userId)
+	{
+		kinect.onNewUser(userId);
+	}
+	
+	public void onLostUser(int userId)
+	{
+		kinect.onLostUser(userId);
+	}
+	
+	public void onStartCalibration(int userId)
+	{
+		kinect.onStartCalibration(userId);
+	}
+	
+	public void onEndCalibration(int userId, boolean successfull)
+	{
+		kinect.onEndCalibration(userId, successfull);
+	}
+	
+	public void onStartPose(String pose,int userId)
+	{
+		kinect.onStartPose(pose, userId);
+	}
+	
+	public void onEndPose(String pose,int userId)
+	{
+		kinect.onEndPose(pose, userId);
 	}
 
 }
---
+++ src/com/lbi/internetweek/views/KinectWrapper.java
@@ -0,0 +1,190 @@
+package com.lbi.internetweek.views;
+
+import SimpleOpenNI.*;
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class KinectWrapper
+{
+	Boolean			DRAW_DEPTH_IMAGE		=	false;
+	Boolean			DRAW_SKELETON			=	true;
+	
+	PApplet			_parent;
+	
+	SimpleOpenNI    _context;
+	
+	PImage          _depthImage;
+	
+	int[]           _rawDepth;
+	int             _kWidth, _kHeight;
+	int             _nearThreshold, _farThreshold;
+	
+	// --------------------------------------------------------------------------------------------------------
+	// SETUP FUNCTIONS
+	// --------------------------------------------------------------------------------------------------------
+	
+	public KinectWrapper(PApplet parent) 
+	{
+		_parent				=	parent;
+		
+		_nearThreshold      =  	0;
+		_farThreshold       =  	2500;
+		
+		setupKinect();
+	}
+
+	private void setupKinect() 
+	{
+		_context = new SimpleOpenNI(_parent,SimpleOpenNI.RUN_MODE_MULTI_THREADED); 
+		_context.setMirror(true);
+		_context.enableDepth();
+		_context.enableUser(SimpleOpenNI.SKEL_PROFILE_ALL);
+
+		_depthImage   		=	_parent.createImage( 320, 240, _parent.RGB );
+	}
+	
+	// --------------------------------------------------------------------------------------------------------
+	// PUBLIC FUNCTIONS
+	// --------------------------------------------------------------------------------------------------------
+	
+	public void update() 
+	{
+		_context.update();		
+	}
+	
+	public void draw()
+	{
+		
+		int i, j, c, dpos, pos;
+		int w=640, h=480;
+		  
+		if( _parent.frameCount % 4 == 0 )
+		{
+			dpos = 0;
+			pos = 0;
+		    
+		    _rawDepth = _context.depthMap();
+		    
+		    for( i = 0; i < h; i += 2 )
+		    {
+		    	for( j = 0; j < w; j += 2 )
+		    	{
+		    		dpos   =  ( i * w ) + j;
+		        
+		    		c      =  _rawDepth[dpos];
+		                
+		    		if( c > _nearThreshold && c < _farThreshold )
+		    			_depthImage.pixels[pos] = 0xffffff;
+		    		else
+		    			_depthImage.pixels[pos] = 0;
+		        
+		    		pos++;
+		    	}
+		    }
+		    
+		    _depthImage.updatePixels();
+		}
+		  
+		if( DRAW_DEPTH_IMAGE )
+		{
+			//draw depthImage quickly on screen
+			_parent.beginShape(_parent.QUADS);
+			_parent.texture(_depthImage);
+			_parent.vertex(0,0,0,0);
+			_parent.vertex(320,0,320,0);
+			_parent.vertex(320,240,320,240);
+			_parent.vertex(0,240,0,240);
+			_parent.endShape();
+		}
+		  
+		if( DRAW_SKELETON )
+		{
+			if(_context.isTrackingSkeleton(1))
+				drawSkeleton(1);
+		}
+	}
+	
+	// --------------------------------------------------------------------------------------------------------
+	// SKELETAL
+	// --------------------------------------------------------------------------------------------------------
+	
+	private void drawSkeleton(int userId) 
+	{
+		_parent.stroke(0,255,0);
+		_parent.strokeWeight(3);
+		
+		_context.drawLimb(userId, SimpleOpenNI.SKEL_HEAD, SimpleOpenNI.SKEL_NECK);
+
+		_context.drawLimb(userId, SimpleOpenNI.SKEL_NECK, SimpleOpenNI.SKEL_LEFT_SHOULDER);
+		_context.drawLimb(userId, SimpleOpenNI.SKEL_LEFT_SHOULDER, SimpleOpenNI.SKEL_LEFT_ELBOW);
+		_context.drawLimb(userId, SimpleOpenNI.SKEL_LEFT_ELBOW, SimpleOpenNI.SKEL_LEFT_HAND);
+
+		_context.drawLimb(userId, SimpleOpenNI.SKEL_NECK, SimpleOpenNI.SKEL_RIGHT_SHOULDER);
+		_context.drawLimb(userId, SimpleOpenNI.SKEL_RIGHT_SHOULDER, SimpleOpenNI.SKEL_RIGHT_ELBOW);
+		_context.drawLimb(userId, SimpleOpenNI.SKEL_RIGHT_ELBOW, SimpleOpenNI.SKEL_RIGHT_HAND);
+
+		_context.drawLimb(userId, SimpleOpenNI.SKEL_LEFT_SHOULDER, SimpleOpenNI.SKEL_TORSO);
+		_context.drawLimb(userId, SimpleOpenNI.SKEL_RIGHT_SHOULDER, SimpleOpenNI.SKEL_TORSO);
+		
+		_parent.noStroke();
+	}
+	
+	public void onNewUser(int userId)
+	{
+	  _parent.println("onNewUser - userId: " + userId);
+	  _parent.println("--Start pose detection");
+	  
+	  _context.startPoseDetection("Psi",userId);
+	}
+
+	public void onLostUser(int userId)
+	{
+	  _parent.println("onLostUser - userId: " + userId);
+	}
+
+	public void onStartCalibration(int userId)
+	{
+	  _parent.println("onStartCalibration - userId: " + userId);
+	}
+
+	public void onEndCalibration(int userId, boolean successfull)
+	{
+	  _parent.println("onEndCalibration - userId: " + userId + ", successfull: " + successfull);
+	  
+	  if (successfull) 
+	  { 
+	    _parent.println("--User calibrated !!!");
+	    _context.startTrackingSkeleton(userId); 
+	  } 
+	  else 
+	  { 
+	    _parent.println("--Failed to calibrate user !!!");
+	    _parent.println("--Start pose detection");
+	    _context.startPoseDetection("Psi",userId);
+	  }
+	}
+
+	public void onStartPose(String pose,int userId)
+	{
+	  _parent.println("onStartPose - userId: " + userId + ", pose: " + pose);
+	  _parent.println("--Stop pose detection");
+	  
+	  _context.stopPoseDetection(userId); 
+	  _context.requestCalibrationSkeleton(userId, true);
+	 
+	}
+
+	public void onEndPose(String pose,int userId)
+	{
+	  _parent.println("onEndPose - userId: " + userId + ", pose: " + pose);
+	}
+	
+	// --------------------------------------------------------------------------------------------------------
+	// GETTER SETTER
+	// --------------------------------------------------------------------------------------------------------
+
+	public PImage getDepthImage() 
+	{
+		return _depthImage;
+	}
+}
